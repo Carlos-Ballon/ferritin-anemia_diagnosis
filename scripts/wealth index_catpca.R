40901a883f0b6a2bb38a46ffca2f56c9dbbec054
Carlos-Ballon <carloebs@gmail.com>
2026-06-07 22:19:40 -0500

    10th commit: CATPCA

--- scripts/wealth index_catpca.R
+++ scripts/wealth index_catpca.R
@@ -1,4 +1,4 @@
-# NOTE: The crucial assumption in the Filmer and Pritchett (2001) study is that a household's long-run wealth explains the maximum variance (and covariance) in the asset variables. https://doi.org/10.1016/j.heliyon.2021.e06163
+# NOTE: The crucial assumption in the Filmer and Pritchett (2001) study is that a household's long-run wealth explains the maximum variance (and covariance) in the asset variables. From: Tareq M, Abdel-Razzaq AI, Rahman MA, Choudhury T. Comparison of weighted and unweighted methods of wealth indices for assessing SOCIO-ECONOMIC status. Heliyon. 2021 Feb 26;7(2):e06163. doi: 10.1016/j.heliyon.2021.e06163. 
 
 # Select PCA variables, recode, and handle missing values
 data_catpca <- data |>
